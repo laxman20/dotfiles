74fcf2d861eff7277a37024cc38dde4c0c381fea
Laxman Sooriyathas <laxman_sooriyathas@ultimatesoftware.com>
2020-08-27 12:23:49 -0400

    First commit

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# setup directories
+mkdir ~/.config
+mkdir ~/.cache
+mkdir ~/.local
+mkdir ~/.bin
+mkdir ~/.local/share
+
+# install homebrew
+mkdir ~/.local/bin/homebrew
+git clone https://github.com/Homebrew/brew.git ~/.local/bin/homebrew
+
+mkdir ~/.config/zsh
+cat <<EOF > ~/.config/zsh/config.zsh
+export HOMEBREW_HOME=$HOME/.local/bin/homebrew
+export HOMEBREW_NO_ANALYTICS=1
+export PATH=$HOMEBREW_HOME/brew/bin:$PATH.
+EOF
---
+++ zsh/.zshrc
@@ -0,0 +1,10 @@
+source ~/.config/zsh/config.zsh
+source ~/.zsh/antigen.zsh
+
+antigen bundle zsh-users/zsh-autosuggestions
+
+antigen apply
+
+alias vim='nvim'
+
+alias gst='git status'
