455d2d9a24502effa5ebae11abca760b3bc13342
Laxman Sooriyathas <laxman.sooriyathas@ukg.com>
2023-10-05 13:36:33 -0400

    add path completion

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -16,6 +16,8 @@ return { "hrsh7th/nvim-cmp",
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
 				['<C-f>'] = cmp.mapping.scroll_docs(4),
 				['<C-Space>'] = cmp.mapping.complete(),
