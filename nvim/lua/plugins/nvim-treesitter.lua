ecaccaf7e335083c8959372f4a6bffd514ba2c54
Laxman Sooriyathas <laxman.sooriyathas@ukg.com>
2023-10-04 20:44:23 -0400

    more plugins

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,10 @@
 return {'nvim-treesitter/nvim-treesitter',
 	build = ':TSUpdate',
+	event = { "VeryLazy" },
+	dependencies = {
+		{ "nvim-treesitter/nvim-treesitter-textobjects", },
+	},
+	cmd = { "TSUpdateSync" },
 	config = function(_, opts) 
 		local configs = require("nvim-treesitter.configs")
 		configs.setup(opts)
@@ -12,5 +17,14 @@ return {'nvim-treesitter/nvim-treesitter',
 		indent = {
 			enable = true
 		},
-	}
+		textobjects = {
+			move = {
+				enable = true,
+				goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+				goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+				goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+				goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+			},
+		},
+	},
 }
