337e767873d998ce8dbdbf1f090ccbc13f73e154
Laxman Sooriyathas <laxman.sooriyathas@ukg.com>
2023-10-17 10:16:28 -0400

    update lualine themme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,9 +1,9 @@
 return { "nvim-lualine/lualine.nvim",
 	config = function()
 		require("lualine").setup({
-			options = {
-				theme = "onedark"
-			}
+			-- options = {
+			-- 	theme = "onedark"
+			-- }
 		})
 	end
 }
