9228925113d87d01f93bc872ee81274ad46364bf
Laxman <laxman0202@gmail.com>
2023-11-24 15:16:45 -0500

    wezterm edit scrollback

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,6 +16,40 @@ config.window_padding = {
 -- config.color_scheme = 'tokyonight_moon'
 config.color_scheme = 'Sonokai (Gogh)'
 
+
+
+w.on('trigger-vim-with-visible-text', function(window, pane)
+	-- Retrieve the current viewport's text.
+	--
+	-- Note: You could also pass an optional number of lines (eg: 2000) to
+	-- retrieve that number of lines starting from the bottom of the viewport.
+	local viewport_text = pane:get_lines_as_text(2000)
+
+	-- Create a temporary file to pass to vim
+	local name = os.tmpname()
+	local f = io.open(name, 'w+')
+	f:write(viewport_text)
+	f:flush()
+	f:close()
+
+	-- Open a new window running vim and tell it to open the file
+	window:perform_action(
+		w.action.SpawnCommandInNewTab {
+			args = { '/opt/homebrew/bin/nvim', name },
+		},
+		pane
+	)
+
+	-- Wait "enough" time for vim to read the file before we remove it.
+	-- The window creation and process spawn are asynchronous wrt. running
+	-- this script and are not awaitable, so we just pick a number.
+	--
+	-- Note: We don't strictly need to remove this file, but it is nice
+	-- to avoid cluttering up the temporary directory.
+	w.sleep_ms(1000)
+	os.remove(name)
+end)
+
 local function is_vim(pane)
 	return pane:get_user_vars().IS_VIM == 'true'
 end
@@ -108,6 +142,11 @@ config.keys = {
 		action = w.action.ShowLauncherArgs {
 			flags = 'WORKSPACES',
 		},
+	},
+	{
+		key = 'E',
+		mods = 'CTRL',
+		action = w.action.EmitEvent 'trigger-vim-with-visible-text',
 	},
 }
 
